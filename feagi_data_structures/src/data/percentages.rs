967ec7aa217a118232ad7cd9cb31d4ab3aecf2a7
Amir Rasteg <amir.rasteg@protonmail.com>
2025-09-22 12:00:46 -0400

    remote specific implementations

--- feagi_data_structures/src/data/percentages.rs
+++ feagi_data_structures/src/data/percentages.rs
@@ -2,7 +2,6 @@ use crate::{define_signed_percentage, define_unsigned_percentage, define_2d_sign
 use crate::FeagiDataError;
 
 
-define_unsigned_percentage!(Percentage, "A percentage value, from 0 to 100%");
 define_signed_percentage!(SignedPercentage, "A signed percentage value, from -100 to 100%");
 define_2d_signed_or_unsigned_percentages!(Percentage2D, Percentage, "Percentage2D", "Represents 2 percentages over 2 dimensions, going from 0 - 100%");
 define_2d_signed_or_unsigned_percentages!(SignedPercentage2D, SignedPercentage, "SignedPercentage2D", "Represents 2 signed percentages over 2 dimensions, going from -100 - 100%");
@@ -10,3 +9,152 @@ define_3d_signed_or_unsigned_percentages!(Percentage3D, Percentage, "Percentage3
 define_3d_signed_or_unsigned_percentages!(SignedPercentage3D, SignedPercentage, "SignedPercentage3D", "Represents 3 signed percentages over 3 dimensions, going from -100 - 100%");
 define_4d_signed_or_unsigned_percentages!(Percentage4D, Percentage, "Percentage4D", "Represents 4 percentages over 4 dimensions, going from 0 - 100%");
 define_4d_signed_or_unsigned_percentages!(SignedPercentage4D, SignedPercentage, "SignedPercentage4D", "Represents 4 signed percentages over 4 dimensions, going from -100 - 100%");
+
+
+//region Percentage (0 - 1)
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Percentage {
+    value: f32,
+}
+
+impl Percentage {
+
+//region Constructors
+
+    pub fn new_from_0_1_unchecked(value: f32) -> Self {
+        Percentage { value }
+    }
+
+    pub fn new_from_0_1(value: f32) -> Result<Percentage, FeagiDataError> {
+    if value > 1.0 || value < 0.0 {
+    return Err(FeagiDataError::BadParameters("Percentage Value must be between 0 and 1!".into()));
+    }
+        Ok(Percentage { value })
+    }
+
+    pub fn new_from_interp_m1_1(value: f32) -> Result<Percentage, FeagiDataError> {
+    if value > 1.0 || value < -1.0 {
+    return Err(FeagiDataError::BadParameters("Signed Percentage Value to interp from must be between -1 and 1!".into()));
+    }
+        Ok(Percentage { value: (value + 1.0) / 2.0 })
+    }
+
+    pub fn new_from_interp_m1_1_unchecked(value: f32) -> Self {
+        Percentage { value: (value + 1.0) / 2.0 }
+    }
+
+    pub fn new_from_u8_0_255(value: u8) -> Result<Percentage, FeagiDataError> {
+        Percentage::new_from_0_1(value as f32 / u8::MAX as f32)
+    }
+
+    pub fn new_from_0_100(value: f32) -> Result<Percentage, FeagiDataError> {
+        if value > 100.0 || value < 0.0 {
+            return Err(FeagiDataError::BadParameters("Percentage Value must be between 0 and 100!".into()));
+        }
+        Ok(Percentage { value: value / 100.0 })
+    }
+
+    pub fn new_from_linear_interp(value: f32, range: &std::ops::Range<f32>) -> Result<Percentage, FeagiDataError> {
+        if value < range.start || value > range.end {
+            return Err(FeagiDataError::BadParameters(format!("Given value {} is out of range {:?}!", value, range)));
+        }
+        Ok(Percentage { value: Self::linear_interp(value, range) })
+    }
+
+//endregion
+
+//region Update
+
+    pub(crate) fn inplace_update(&mut self, value: f32)  {
+        self.value = value;
+    }
+
+    pub fn inplace_update_from_0_1(&mut self, value: f32) -> Result<(), FeagiDataError> {
+        if value > 1.0 || value < 0.0 {
+            return Err(FeagiDataError::BadParameters("Percentage Value must be between 0 and 1!".into()));
+        }
+        self.value = value;
+        Ok(())
+    }
+
+    pub fn inplace_update_u8_0_255(&mut self, value: u8) -> Result<(), FeagiDataError> {
+        self.inplace_update_from_0_1(value as f32 / u8::MAX as f32)
+    }
+
+    pub fn inplace_update_0_100(&mut self, value: f32) -> Result<(), FeagiDataError> {
+        if value > 100.0 || value < 0.0 {
+            return Err(FeagiDataError::BadParameters("Percentage Value must be between 0 and 100!".into()));
+        }
+        self.value = value / 100.0;
+        Ok(())
+    }
+
+    pub fn inplace_update_linear_interp(&mut self, value: f32, range: &std::ops::Range<f32>) -> Result<(), FeagiDataError> {
+        if value < range.start || value > range.end {
+            return Err(FeagiDataError::BadParameters(format!("Given value {} is out of range {:?}!", value, range)));
+        }
+        self.value = Self::linear_interp(value, range);
+        Ok(())
+    }
+
+//endregion
+
+//region Properties
+
+    pub fn get_as_0_1(&self) -> f32 {
+        self.value
+    }
+
+    pub fn get_as_u8(&self) -> u8 {
+        (self.value * u8::MAX as f32) as u8
+    }
+
+    pub fn get_as_0_100(&self) -> f32 {
+        self.value * 100.0
+    }
+
+//endregion
+
+//region Internal
+
+    #[inline]
+    fn linear_interp(input: f32, range: &std::ops::Range<f32>) -> f32 {
+        (input - range.start) / (range.end - range.start)
+    }
+
+//endregion
+
+}
+
+impl std::fmt::Display for Percentage {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Percent({} %)", self.get_as_0_100())
+    }
+}
+
+impl TryFrom<f32> for Percentage {
+type Error = FeagiDataError;
+    fn try_from(value: f32) -> Result<Self, Self::Error> {
+        Percentage::new_from_0_1(value)
+    }
+}
+
+impl TryFrom<&f32> for Percentage {
+    type Error = FeagiDataError;
+    fn try_from(value: &f32) -> Result<Self, Self::Error> {
+        Percentage::new_from_0_1(*value)
+    }
+}
+
+impl From<Percentage> for f32 {
+    fn from(value: Percentage) -> Self {
+        value.value
+    }
+}
+
+impl From<&Percentage> for f32 {
+    fn from(value: &Percentage) -> Self {
+        value.value
+    }
+}
+//endregion
