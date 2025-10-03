1eddee5efaff4786d97fb05a98ed470a0a755575
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-03 14:31:58 -0400

    stiuff

---
+++ feagi_connector_core/src/data_pipeline/stage_properties/image_segmentor.rs
@@ -0,0 +1,76 @@
+use std::any::Any;
+use feagi_data_structures::FeagiDataError;
+use crate::data_pipeline::pipeline_stage_properties::PipelineStageProperties;
+use crate::data_pipeline::PipelineStage;
+use crate::data_pipeline::stages::ImageFrameSegmentatorStage;
+use crate::data_types::descriptors::{ImageFrameProperties, SegmentedImageFrameProperties};
+use crate::data_types::ImageFrameSegmentator;
+use crate::wrapped_io_data::WrappedIOType;
+
+/// Properties for ImageFrameSegmentatorStage that store configuration for image segmentation
+#[derive(Debug, Clone)]
+pub struct ImageSegmentorStageProperties {
+    input_image_properties: ImageFrameProperties,
+    output_image_properties: SegmentedImageFrameProperties,
+    image_segmentator: ImageFrameSegmentator,
+}
+
+impl PipelineStageProperties for ImageSegmentorStageProperties {
+    fn get_input_data_type(&self) -> WrappedIOType {
+        WrappedIOType::ImageFrame(Some(self.input_image_properties))
+    }
+
+    fn get_output_data_type(&self) -> WrappedIOType {
+        WrappedIOType::SegmentedImageFrame(Some(self.output_image_properties))
+    }
+
+    fn clone_box(&self) -> Box<dyn PipelineStageProperties> {
+        Box::new(self.clone())
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn create_stage(&self) -> Box<dyn PipelineStage> {
+        ImageFrameSegmentatorStage::new_box(
+            self.input_image_properties,
+            self.output_image_properties,
+            self.image_segmentator.clone()
+        ).unwrap()
+    }
+}
+
+impl ImageSegmentorStageProperties {
+    pub fn new(
+        input_image_properties: ImageFrameProperties,
+        output_image_properties: SegmentedImageFrameProperties,
+        image_segmentator: ImageFrameSegmentator
+    ) -> Self {
+        ImageSegmentorStageProperties {
+            input_image_properties,
+            output_image_properties,
+            image_segmentator,
+        }
+    }
+    
+    pub fn new_box(
+        input_image_properties: ImageFrameProperties,
+        output_image_properties: SegmentedImageFrameProperties,
+        image_segmentator: ImageFrameSegmentator
+    ) -> Result<Box<dyn PipelineStageProperties + Send + Sync + 'static>, FeagiDataError> {
+        Ok(Box::new(ImageSegmentorStageProperties::new(
+            input_image_properties,
+            output_image_properties,
+            image_segmentator
+        )))
+    }
+}
+
+impl std::fmt::Display for ImageSegmentorStageProperties {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "ImageSegmentorStage(input: {:?}, output: {:?})", 
+               self.input_image_properties, self.output_image_properties)
+    }
+}
+
--- feagi_connector_core/src/data_pipeline/stage_properties/mod.rs
+++ feagi_connector_core/src/data_pipeline/stage_properties/mod.rs
@@ -1,5 +1,7 @@
 mod identities;
+mod image_segmentor;
 //mod image_quick_diff;
 
 pub use identities::IdentityStageProperties;
+pub use image_segmentor::ImageSegmentorStageProperties;
 //pub use image_quick_diff::ImageQuickDiffStageProperties;
