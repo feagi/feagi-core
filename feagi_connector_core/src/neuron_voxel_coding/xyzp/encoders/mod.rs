d10cba5167d09c522fc370f91126e5caf49b3526
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-09 21:50:45 -0400

    linear

--- feagi_connector_core/src/neuron_voxel_coding/xyzp/encoders/mod.rs
+++ feagi_connector_core/src/neuron_voxel_coding/xyzp/encoders/mod.rs
@@ -7,14 +7,18 @@ mod segmented_image_frame;
 mod percentage_fractional_exponential;
 mod signed_percentage_fractional_exponential;
 */
-
+// TODO there is a bug on all encoders in that they will send all channel data even if not updated since scratch is cleared only if channel is active!
 mod misc_data;
 mod image_frame;
 mod segmented_image_frame;
 mod percentage_1d_exponential;
+mod percentage_1d_linear;
 mod percentage_2d_exponential;
+mod percentage_2d_linear;
 mod percentage_3d_exponential;
+mod percentage_3d_linear;
 mod percentage_4d_exponential;
+mod percentage_4d_linear;
 /*
 pub use image_frame::{ImageFrameNeuronVoxelXYZPEncoder};
 pub use signed_percentage_split_sign_divided::{F32SplitSignDividedNeuronVoxelXYZPEncoder};
@@ -30,6 +34,10 @@ pub(crate) use misc_data::MiscDataNeuronVoxelXYZPEncoder;
 pub(crate) use image_frame::ImageFrameNeuronVoxelXYZPEncoder;
 pub(crate) use segmented_image_frame::SegmentedImageFrameNeuronVoxelXYZPEncoder;
 pub(crate) use percentage_1d_exponential::Percentage1DExponentialNeuronVoxelXYZPEncoder;
+pub(crate) use percentage_1d_linear::Percentage1DLinearNeuronVoxelXYZPEncoder;
 pub(crate) use percentage_2d_exponential::Percentage2DExponentialNeuronVoxelXYZPEncoder;
+pub(crate) use percentage_2d_linear::Percentage2DLinearNeuronVoxelXYZPEncoder;
 pub(crate) use percentage_3d_exponential::Percentage3DExponentialNeuronVoxelXYZPEncoder;
+pub(crate) use percentage_3d_linear::Percentage3DLinearNeuronVoxelXYZPEncoder;
 pub(crate) use percentage_4d_exponential::Percentage4DExponentialNeuronVoxelXYZPEncoder;
+pub(crate) use percentage_4d_linear::Percentage4DLinearNeuronVoxelXYZPEncoder;
