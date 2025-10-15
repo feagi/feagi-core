30c16ad588eda3648b60f46a4ffbea53cd5e37d3
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-15 17:42:17 -0400

    whoops

--- feagi_connector_core/src/data_pipeline/stage_properties/image_segmentor.rs
+++ feagi_connector_core/src/data_pipeline/stage_properties/image_segmentor.rs
@@ -89,7 +89,7 @@ impl std::fmt::Display for ImageSegmentorStageProperties {
 
 impl<'a> TryFrom<&'a Box::<dyn PipelineStageProperties>> for &'a ImageSegmentorStageProperties {
     type Error = FeagiDataError;
-    fn try_from(value: &Box::<dyn PipelineStageProperties>) -> Result<Self, Self::Error> {
+    fn try_from(value: &'a Box::<dyn PipelineStageProperties>) -> Result<Self, Self::Error> {
         match value.as_any().downcast_ref::<ImageSegmentorStageProperties>() {
             Some(p) => Ok(p),
             None => Err(FeagiDataError::InternalError("Given stage attempted to be cast as '&ImageSegmentorStageProperties' when it isn't!".into()))
