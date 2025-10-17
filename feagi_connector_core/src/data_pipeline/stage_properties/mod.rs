a934aa1df5b47cb0ea5fd470678f6e45973ac689
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-17 19:51:10 -0400

    readd image diff

--- feagi_connector_core/src/data_pipeline/stage_properties/mod.rs
+++ feagi_connector_core/src/data_pipeline/stage_properties/mod.rs
@@ -7,8 +7,10 @@
 
 mod identities;
 mod image_segmentor;
+mod image_quick_diff;
 //mod image_quick_diff;
 
 pub use identities::IdentityStageProperties;
 pub use image_segmentor::ImageSegmentorStageProperties;
+pub use image_quick_diff::ImageQuickDiffStageProperties;
 //pub use image_quick_diff::ImageQuickDiffStageProperties;
