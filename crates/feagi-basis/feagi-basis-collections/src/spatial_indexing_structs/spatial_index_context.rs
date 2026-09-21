8c30aaaef060e9cbc6f5c475571ac9153d38290e
amir.rasteg <amir.rasteg@protonmail.com>
2026-09-21 18:40:39 -0400

    a

--- crates/feagi-basis/feagi-basis-collections/src/spatial_indexing_structs/spatial_index_context.rs
+++ crates/feagi-basis/feagi-basis-collections/src/spatial_indexing_structs/spatial_index_context.rs
@@ -6,6 +6,10 @@ use crate::spatial_indexing_structs::spatial_index_mapper::SpatialIndexMapper;
 use crate::spatial_indexing_structs::SpatialStride;
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(bound(
+    serialize = "SpatialDimensions<QDims, NUM_DIMS>: Serialize, AxisOrderArray<NUM_DIMS>: Serialize, SpatialStride<NUM_DIMS>: Serialize",
+    deserialize = "SpatialDimensions<QDims, NUM_DIMS>: Deserialize<'de>, AxisOrderArray<NUM_DIMS>: Deserialize<'de>, SpatialStride<NUM_DIMS>: Deserialize<'de>"
+))]
 pub struct SpatialIndexContext<QDims, const NUM_DIMS: usize>
 where
     QDims: QuantizedUnsignedIntegerTrait
--- crates/feagi-basis/feagi-basis-neuron/src/single_area_collections/voxels/cortical_area_voxels.rs
+++ crates/feagi-basis/feagi-basis-neuron/src/single_area_collections/voxels/cortical_area_voxels.rs
@@ -1,10 +1,11 @@
+use serde::{Deserialize, Serialize};
 use feagi_basis_collections::generic_data::par_data::GenericParData;
 
 use feagi_basis_collections::spatial_indexing_structs::spatial_index_context::SpatialIndexContext;
 use feagi_basis_quantization::prelude::*;
 use crate::single_area_collections::voxels::context::{CorticalAreaVoxelCoordinateAxisIndex, CorticalAreaVoxelLinearIndex, CorticalAreaVoxelPotential};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Serialize)]
 pub struct CorticalAreaVoxelData<
     QI: QuantizedUnsignedIntegerUnwrappedTrait,
     QP: QuantizedDecimalUnwrappedTrait,
